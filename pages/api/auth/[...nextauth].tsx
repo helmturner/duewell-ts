89dde16cc6eb8de248593412a47e8be943a50562
alecvision <git@alecvision.com>
2022-01-27 21:39:31 -0600

    fix types, refactor next-auth callbacks

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth";
-import EmailProvider from "next-auth/providers/email";
+import NextAuth, { Account, Profile, Session, User } from "next-auth";
+// import EmailProvider from "next-auth/providers/email";
 import GitHubProvider from "next-auth/providers/github";
-
+import type { JWT } from "next-auth/jwt";
 
 /*TODO: Move secrets to .env && add additional providers including:
 magic link, credentials (with Auth0 as auth server), google, apple, etc*/
@@ -23,37 +23,50 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
-    async jwt({ token, account, isNewUser, profile, user }) {
-      console.log("Token: ", JSON.stringify(token));
-      console.log("Account: ", JSON.stringify(account));
-      console.log("Profile: ", JSON.stringify(profile));
-      console.log("User: ", JSON.stringify(user));
-      console.log("New?: ", JSON.stringify(isNewUser));
-/*       if (account) {
+    async jwt(
+      params: { token: JWT, account?: Account, profile?: Profile, isNewUser?: Boolean, user?: User}
+    ) {
+      let {token, account, profile, isNewUser, user} = params
+
+      if (isNewUser) {
+        //TODO: Check that new accounts do not have emails already tied to another account
+      }
+
+      if (account) {
         token.account = {
-          type: account.type,
-          id: account.providerAccountId,
+          id: String(account.type) + " | " + String(account.provider) + " | " + String(account.providerAccountId),
           accessToken: account.access_token,
-          idToken: account.id_token,
-          scope: account.scope,
-          expiry: account.expires_at,
           tokenType: account.token_type
         }
       }
 
-      if (profile && user) {
+      if (user) {
         token.user = {
-          username: user.name,
-          email: profile.email, 
-          isNew: isNewUser || false,
+          name: user.name,
+          email: user.email,
+          image: user.image,
+          isNew: isNewUser || false
+        }
+      }
+      if (profile) {
+        token.profile = {
           verified: profile.email_verified,
           aud: profile.aud,
           iat: profile.iat,
           exp: profile.exp
         }
       }
-      console.log(JSON.stringify(token)) */
+      console.log(JSON.stringify(token)); //DEBUG: console log token
       return token
+    }, 
+    async session(
+      { session, token, user }: { session: Session, token: JWT, user: User }
+    ): Promise<Session & { id: string }> {
+      // Send properties to the client, like an access_token from a provider.
+      let account = token.account as Account;
+      session.accessToken = token.accessToken
+      session.id = {...user, id: account.id }
+      return session as Session & {id: string}
     }
   }
 })
