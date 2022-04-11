313a9929195c1ada669262c6980f345c82d7c9e1
Alec Helmturner <git@alecvision.com>
2022-04-11 12:23:46 -0500

    integrate external api, update eslint config

---
+++ src/pages/api/plaid/[[...item_id]].ts
@@ -0,0 +1,181 @@
+import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+import { db, plaidClient } from "server/db";
+import { PlaidItemModel } from "server/db/types";
+import {
+  isValidItemStatus,
+  sanitizeItems,
+  validItemStatuses,
+} from "server/utils";
+
+const teapot: NextApiHandler = (_, res) => {
+  res.status(418);
+};
+
+const todo: NextApiHandler = (_, res) => {
+  res.status(501);
+};
+
+const handler: Record<string, NextApiHandler> = {
+  options: todo,
+  patch: todo,
+  default: teapot,
+  head: teapot,
+  connect: teapot,
+  trace: teapot,
+  put: async (req, res) => {
+    const session = getSession(req, res);
+
+    if (!session || !session.idToken || !session.user || !session.accessToken) {
+      res.status(401);
+      return;
+    }
+
+    const item_id = req.query.item_id[0];
+    const { status } = req.body;
+
+    if (status) {
+      if (!isValidItemStatus(status)) {
+        res.status(400).json({
+          reason: `${status} is not a valid Item status`,
+          acceptedValues: validItemStatuses,
+        });
+      }
+      await db.plaidItems.update({ item_id }, { status });
+      const item = await db.plaidItems.read({ item_id });
+      res.status(200).json(sanitizeItems(item));
+    } else {
+      res.status(400).json({
+        reason: "You must provide updated item information.",
+        acceptedKeys: ["status"],
+      });
+    }
+  },
+  delete: async (req, res) => {
+    const session = getSession(req, res);
+
+    if (!session || !session.idToken || !session.user || !session.accessToken) {
+      res.status(401);
+      return;
+    }
+
+    let removed: boolean;
+    let status_code: number;
+    const item_id = req.query.item_id[0];
+
+    const item = await db.plaidItems.read({ item_id });
+    const accessToken = item?.access_token;
+    const response = await plaidClient.itemRemove({
+      access_token: accessToken || "",
+    });
+    removed = response.data.removed;
+    status_code = response.data.status_code;
+
+    if (!removed) return res.status(status_code).send("Failed to delete item");
+
+    await db.plaidItems.delete({ item_id });
+    res.status(204).send("Item successfully deleted");
+  },
+  get: async (req, res) => {
+    const session = getSession(req, res);
+
+    if (!session || !session.idToken || !session.user || !session.accessToken) {
+      res.status(401);
+      return;
+    }
+
+    const item_id = req.query.item_id[0];
+    try {
+      const item = await db.plaidItems.read({ item_id });
+      res.status(200).json(sanitizeItems(item));
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  post: async (req, res) => {
+    try {
+      const session = getSession(req, res);
+
+      if (
+        !session ||
+        !session.idToken ||
+        !session.user ||
+        !session.accessToken
+      ) {
+        res.status(401);
+        return;
+      }
+
+      const {
+        user,
+        idToken,
+        accessToken,
+        accessTokenScope,
+        accessTokenExpiresAt,
+        refreshToken,
+      } = session;
+
+      const { institution_id, public_token } = req.body;
+      const user_id = user.sub as string;
+
+      console.info("user~", user);
+      console.info("idToken~", idToken);
+      console.info("accessToken~", accessToken);
+      console.info("accessTokenExpiresAt~", accessTokenExpiresAt);
+      console.info("accessTokenScope~", accessTokenScope);
+      console.info("refreshToken", refreshToken);
+      console.warn("USER AUTH NOT FULLY IMPLEMENTED");
+
+      //TODO: if session token scopes are not sufficient, return 403 or 404
+      try {
+        const existingItem: PlaidItemModel | null = await db.plaidItems.read({
+          institution_id,
+          user_id,
+        });
+
+        if (existingItem) {
+          res.status(409).send("You have already linked this institution.");
+          return;
+        }
+
+        const plaidResponse = await plaidClient.itemPublicTokenExchange({
+          public_token,
+        });
+
+        if (!plaidResponse.data) {
+          res.status(plaidResponse.status).json(plaidResponse); //XXX
+        }
+
+        const { acknowledged, insertedId } = await db.plaidItems.create({
+          ...plaidResponse.data,
+          institution_id,
+          user_id,
+        });
+
+        if (!acknowledged) {
+          res.status(500);
+        }
+
+        const newItem = await db.plaidItems.read({ _id: insertedId });
+
+        res.status(200).json(sanitizeItems(newItem));
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  },
+};
+
+async function transactionHandler(req: NextApiRequest, res: NextApiResponse) {
+  const method = req.method?.toLowerCase();
+  try {
+    return handler[method!](req, res);
+  } catch {
+    return handler.default(req, res);
+  }
+}
+
+export default withApiAuthRequired(transactionHandler);
--- src/pages/api/plaid/link.ts
+++ src/pages/api/plaid/link.ts
@@ -1,7 +1,8 @@
 import { getSession, withApiAuthRequired } from "@auth0/nextjs-auth0";
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 import { fromJson } from "@auth0/nextjs-auth0/dist/session";
-import { LinkTokenCreateResponse } from "plaid";
+import { LinkTokenCreateRequest, LinkTokenCreateResponse } from "plaid";
+import { db, plaidClient, PLAID_COUNTRY_CODES, PLAID_PRODUCTS, PLAID_REDIRECT_URI } from "server/db";
 
 const externalApiBaseUrl = process.env.EXTERNAL_API_BASE;
 
@@ -24,80 +25,63 @@ const handler: Record<string, NextApiHandler> = {
   connect: teapot,
   trace: teapot,
   post: async (req, res) => {
-    try {
-      const session = fromJson(getSession(req, res)!);
+    const session = fromJson(getSession(req, res)!);
 
-      if (
-        !session ||
-        !session.idToken ||
-        !session.user ||
-        !session.accessToken
-      ) {
-        res.status(401);
-        return;
-      }
-
-      const {
-        user,
-        idToken,
-        accessToken,
-        accessTokenScope,
-        accessTokenExpiresAt,
-        refreshToken,
-      } = session;
-
-      const params = req.body
-
-      console.info("user~", user);
-      console.info("idToken~", idToken);
-      console.info("accessToken~", accessToken);
-      console.info("accessTokenExpiresAt~", accessTokenExpiresAt);
-      console.info("accessTokenScope~", accessTokenScope);
-      console.info("refreshToken", refreshToken);
-      console.warn("USER AUTH NOT FULLY IMPLEMENTED");
-
-      //TODO: if session token scopes are not sufficient, return 403 or 404
-
-      const tokenResponse = await fetch(
-        "https://dev-7aw1wy7g.us.auth0.com/oauth/token",
-        {
-          method: "POST",
-          headers: { "content-type": "application/json" },
-          body: JSON.stringify({
-            client_id: process.env.AUTH0_CLIENT_ID,
-            client_secret: process.env.AUTH0_CLIENT_SECRET,
-            audience: process.env.AUTH0_AUDIENCE,
-            grant_type: "client_credentials",
-          }),
-        }
-      );
-
-      if (tokenResponse.ok) {
-        const { token_type, access_token } = await tokenResponse.json();
-        const linkTokenResponse = await fetch(
-          `${externalApiBaseUrl}/plaid/link`,
-          {
-            method: req.method,
-            headers: {
-              Authorization: `${token_type} ${access_token}`,
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ ...params, user_id: user.sub }),
-          }
-        )
-
-        const linkResponse: LinkTokenCreateResponse = await linkTokenResponse.json();
-        console.log(linkResponse)
-        res.status(linkTokenResponse.status).json(linkResponse);
-      }
-    } catch (error: any) {
-      console.error(error);
-      res.status(error.status || 500).json({
-        code: error.code,
-        error: error.message,
-      });
+    if (!session || !session.idToken || !session.user || !session.accessToken) {
+      res.status(401);
+      return;
     }
+
+    const {
+      user,
+      idToken,
+      accessToken,
+      accessTokenScope,
+      accessTokenExpiresAt,
+      refreshToken,
+    } = session;
+
+    console.info("user~", user);
+    console.info("idToken~", idToken);
+    console.info("accessToken~", accessToken);
+    console.info("accessTokenExpiresAt~", accessTokenExpiresAt);
+    console.info("accessTokenScope~", accessTokenScope);
+    console.info("refreshToken", refreshToken);
+    console.warn("USER AUTH NOT FULLY IMPLEMENTED");
+
+    //TODO: if session token scopes are not sufficient, return 403 or 404
+
+    const { item_id } = req.body;
+    const user_id = user.sub as string;
+    let access_token = null;
+    let products = PLAID_PRODUCTS;
+
+    if (item_id) {
+      // for the link update mode, include access token and an empty products array
+      const itemIdResponse = await db.plaidItems.read({ item_id });
+      access_token = itemIdResponse?.plaid_access_token;
+      products = [];
+    }
+
+    const configs: LinkTokenCreateRequest = {
+      user: {
+        client_user_id: user_id,
+      },
+      client_name: "DueWell",
+      products,
+      country_codes: PLAID_COUNTRY_CODES,
+      language: "en", //TODO: Move to env var in case of internationalization,
+      access_token: access_token,
+    };
+
+    if (PLAID_REDIRECT_URI.indexOf("http") === 0) {
+      configs.redirect_uri = PLAID_REDIRECT_URI;
+    }
+
+    const { data, status, statusText } = await plaidClient.linkTokenCreate(configs);
+    const body = status.toString(10).startsWith("2") ? data : statusText;
+
+    res.status(status).send(body);
   },
 };
 
